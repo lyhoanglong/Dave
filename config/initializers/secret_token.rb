db7219401070672ac98347a1db6895556bfb0348
quyetdc <quyetdc.uet@gmail.com>
2013-10-31 01:58:43 +0700

    initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dave::Application.config.secret_token = '1f1074c9764c05e3b9a8bbbe488606d6cbfe4db5d60e6b44364c997dd67531272f455ce3fc886fbd1e0625669394b890fe3d086b1fbdfd54861454eac5236988'
